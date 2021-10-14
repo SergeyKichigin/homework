d24e34757c4fe08e70d95c5380d914918b54ba9a
Sergey <kichigin-357@mail.ru>
2021-10-14 21:51:20 +0400

    homework 6

--- src/test/java/homework6/ArraysTest.java
+++ src/test/java/homework6/ArraysTest.java
@@ -12,7 +12,7 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ArraysTest {
+class ArrayTest {
     @ParameterizedTest
     @MethodSource("changeArrayParams")
     void changeArray(int[] in, int [] out) {
